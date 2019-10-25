1a277365dad30d6c78412a9ad751c071fbacc78a
Mikanister <53656170+Mikanister@users.noreply.github.com>
2019-10-25 20:55:24 +0300

    Add files via upload

---
+++ USART_TEST/Objects/main.d
@@ -0,0 +1,14 @@
+.\objects\main.o: main.c
+.\objects\main.o: ..\MY_BIBLE\stm32f10x.h
+.\objects\main.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\main.o: ..\MY_BIBLE\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: ..\MY_BIBLE\cmsis_version.h
+.\objects\main.o: ..\MY_BIBLE\cmsis_compiler.h
+.\objects\main.o: ..\MY_BIBLE\cmsis_armcc.h
+.\objects\main.o: ..\MY_BIBLE\system_stm32f10x.h
+.\objects\main.o: ..\MY_BIBLE\stm32f10x_gpio.h
+.\objects\main.o: ..\MY_BIBLE\stm32f10x_rcc.h
+.\objects\main.o: ..\MY_BIBLE\stm32f10x_usart.h
+.\objects\main.o: ..\MY_BIBLE\misc.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
